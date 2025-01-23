a1092fa42c1857d69d36397cc6a27f9536df6a9b
web-mahadihasan <mehedihasanmilu7@gmail.com>
2025-01-23 19:27:54 +0600

    Try to fix issue

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import React from 'react'
 import NavItems from './NavItems'
-import { getKindeServerSession, LoginLink, LogoutLink, RegisterLink } from '@kinde-oss/kinde-auth-nextjs/server';
+// import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
+import { getKindeServerSession, LoginLink, LogoutLink, RegisterLink, isLoading, isAuthenticated } from '@kinde-oss/kinde-auth-nextjs/server';
+// const { user, isLoading, isAuthenticated, login, logout } = useKindeBrowserClient()
 
 const Navbar = async () => {
     const {getUser, isAuthenticated} = getKindeServerSession();
     const user = await getUser();
-    const isAuthenticate = await isAuthenticated();
-    // console.log(user)
+
   return (
     <div className='w-full bg-white/60 sticky top-0 shadow-md z-50 backdrop-blur-xl'>
         <nav className='max-w-7xl mx-auto px-4 py-4 xl:px-0 flex items-center justify-between'>
@@ -16,33 +17,37 @@ const Navbar = async () => {
             </div>
             <div className='flex items-center gap-4'>
                 <NavItems/>
-                <div className='space-x-2'>
-                    {
-                        isAuthenticate ? 
-                        <Link href={"api/auth/logout"}>
+                {
+                    !isLoading && <div className='space-x-2'>
+                        {
+                        isAuthenticated ? 
+                        // <Link href={"api/auth/logout"}>
+                        <LogoutLink>
                             <button
                             className="px-4 py-1.5 border border-[#3e3939] bg-base-100 text-[#000000] hover:bg-[#000000] hover:text-white transition duration-300 rounded-md shadow-md">
                                 Log Out
                             </button>
-                        </Link>
+                        </LogoutLink>
                         : 
                        <> 
-                       <Link href={"api/auth/login"}>
-                        <button
+                       <LoginLink>
+                        <button  onClick={() => login()}
                         className="px-4 py-1.5 border border-[#3e3939] bg-[#000000] text-white hover:bg-transparent hover:text-[#000] transition duration-300 rounded-md shadow-md">
                             Log in
                         </button>
-                        </Link>
+                        </LoginLink>
                         
-                        <Link href={"api/auth/register"}>                       
-                            <button
+                        <RegisterLink>                      
+                            <button onClick={() => login()}
                             className="px-4 py-1.5 border border-[#3e3939] bg-base-100 text-[#000000] hover:bg-[#000000] hover:text-white transition duration-300 rounded-md shadow-md">
                                 Register
                             </button>
-                        </Link>
+                        </RegisterLink>
                     </>
                     }
-                </div>
+                    </div>
+                }
+                
                 
             </div>
         </nav>
